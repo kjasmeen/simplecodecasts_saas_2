567a266aa9b9cd9f903a2c89a6f5b8c702d685e2
Karthika Solai <ksolai92@gmail.com>
2016-03-29 22:28:08 +0000

    Added messages after contact submit and created form validation.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,6 +5,21 @@ class ContactsController < ApplicationController
   end      
   
   def create
+    @contact =Contact.new(contact_params)
+    #the code below white lists the objects in the form. 
+    #must whitelist for security purposes in  
+   
+    if @contact.save
+      flash[:sucess] = "Message sent."
+      redirect_to new_contact_path
+    else
+      flash[:error] = "Error occured. Message has not been sent."
+      redirect_to new_contact_path
+    end
   end      
   
+  private
+    def contact_params
+      params.require(:contact).permit(:name, :email, :comments)
+    end
 end  
